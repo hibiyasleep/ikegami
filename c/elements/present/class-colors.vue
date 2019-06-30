0297888ac9da955808c3f8ba343e5abce037a4a4
Kuriyama Hibiya <kuriyama@hibiya.moe>
2019-07-01 00:50:11 +0900

    gnb/dnc support

--- c/elements/present/class-colors.vue
+++ c/elements/present/class-colors.vue
@@ -3,6 +3,7 @@
     <span class="icon-class class-pld"><span class="ticker"> </span></span>
     <span class="icon-class class-war"><span class="ticker"> </span></span>
     <span class="icon-class class-drk"><span class="ticker"> </span></span>
+    <span class="icon-class class-gnb"><span class="ticker"> </span></span>
     <span class="icon-class class-mnk"><span class="ticker"> </span></span>
     <span class="icon-class class-drg"><span class="ticker"> </span></span>
     <span class="icon-class class-nin"><span class="ticker"> </span></span>
@@ -13,6 +14,7 @@
     <span class="icon-class class-blm"><span class="ticker"> </span></span>
     <span class="icon-class class-rdm"><span class="ticker"> </span></span>
     <span class="icon-class class-blu"><span class="ticker"> </span></span>
+    <span class="icon-class class-dnc"><span class="ticker"> </span></span>
     <span class="icon-class class-whm"><span class="ticker"> </span></span>
     <span class="icon-class class-sch"><span class="ticker"> </span></span>
     <span class="icon-class class-ast"><span class="ticker"> </span></span>
@@ -27,8 +29,8 @@
   margin-bottom: 1rem !important
 
   > span
-    width: 1.375rem
-    height: 1.375rem
+    width: 1.25rem
+    height: 1.25rem
 
   .ticker
     display: inline-block
