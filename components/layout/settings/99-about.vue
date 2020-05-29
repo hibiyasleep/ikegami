0d8154290212c9cc0003350ff76b680481c59c37
Kuriyama Hibiya <kuriyama@hibiya.moe>
2020-05-29 10:14:46 +0900

    settings: add comment in about tab (NOT a sponsored ad)

--- components/layout/settings/99-about.vue
+++ components/layout/settings/99-about.vue
@@ -1,10 +1,17 @@
 <template>
-  <div class="tab-content">
+  <div class="tab-content" style="text-align: center">
     <version />
     <p>
-      © 2018- kuriyama hibiya, all rights reserved.
+      © 2018- hibiya 'inemuri' kuriyama, all rights reserved.
+    </p>
+    <p style="font-size: 0.6em">
+      渋谷・自由が丘・二子玉川を結ぶ東急線を410円で1日自由に行き来できる
       <br />
-      This program is disturibed under GNU GPLv3.
+      「東急トライアングルチケット」
+      <br />
+      東急・西武線各駅の券売機から購入できる
+      <br />
+      ※広告がない　コンセプトに忠実なだけ
     </p>
   </div>
 </template>
