25298745e50737b6dcec8af1fbb245b5fd05c0e8
Kuriyama Hibiya <kuriyama@hibiya.moe>
2020-08-19 03:30:51 +0900

    settings/about: remove tokyu triangle ticket message, against covid-19 situation

--- components/layout/settings/99-about.vue
+++ components/layout/settings/99-about.vue
@@ -4,15 +4,6 @@
     <p>
       © 2018- hibiya 'inemuri' kuriyama, all rights reserved.
     </p>
-    <p style="font-size: 0.6em">
-      渋谷・自由が丘・二子玉川を結ぶ東急線を410円で1日自由に行き来できる
-      <br />
-      「東急トライアングルチケット」
-      <br />
-      東急・西武線各駅の券売機から購入できる
-      <br />
-      ※広告がない　コンセプトに忠実なだけ
-    </p>
   </div>
 </template>
 
