5d5fe84175e59b4c891e70a2194fe576a7272fd9
hibiya inemuri <inemuri@hibiya.moe>
2024-07-02 14:20:21 +0900

    switch to vite

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import packageinfo from './package.json'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue2'
+import { tarball } from 'vite-plugin-compression2'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    vue(),
+    mode === 'production' && tarball({
+      dest: `${packageinfo.name}-${packageinfo.version}`
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': __dirname,
+      //'vue': 'vue/dist/vue.common'
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      sass: {
+        additionalData: `@import '@/styles/_variables.sass'\n`
+      }
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 9000,
+    strictPort: true
+  }
+}))
